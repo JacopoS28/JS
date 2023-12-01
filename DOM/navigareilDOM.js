d837a9cf5df7b7b7058beeba584d72df63bc4f35
JacopoS28 <147383976+JacopoS28@users.noreply.github.com>
2023-12-01 17:26:07 +0100

    Add files via upload
    commit esercizi cercare nel DOM, manipolare il DOM

---
+++ DOM/navigareilDOM.js
@@ -0,0 +1,10 @@
+const html = document.documentElement
+console.log(html);
+const body = ul.parentElement
+console.log(body);
+const ul = body.children[0]
+console.log(ul);
+const myLi = ul.children[1]
+console.log(myLi);
+myLi.nextElementSibling;
+myLi.previousElementSibling;
